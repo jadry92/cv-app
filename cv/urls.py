3bc7bba1500ecdb823a8c551919f8dab2548988b
Johan Suarez <jhoanseb1992@gmail.com>
2024-01-15 16:27:49 +1100

    add cv templates

--- cv/urls.py
+++ cv/urls.py
@@ -2,14 +2,18 @@
 
 # Django
 from django.urls import path
+
 # Views
-from cv.views import CVListView
+from cv.views import CVListView, CVDetailView, CVDeleteView, CVUpdateView, CVCreateView
+
+
+app_name = "cv"
 
 urlpatterns = [
-    path("cvs/", CVListView.as_view(), name="cv_list"),
-    path("cv_create/", CVListView.as_view(), name="cv_create"),
-    path("cv/<int:pk>/", CVListView.as_view(), name="cv_detail"),
-    path("cv/<int:pk>/edit/", CVListView.as_view(), name="cv_edit"),
-    path("cv/<int:pk>/delete/", CVListView.as_view(), name="cv_delete"),
+    path("cv/", CVListView.as_view(), name="cv_list"),
+    path("cv/create/", CVCreateView.as_view(), name="cv_create"),
+    path("cv/<int:pk>/", CVDetailView.as_view(), name="cv_detail"),
+    path("cv/<int:pk>/edit/", CVUpdateView.as_view(), name="cv_edit"),
+    path("cv/<int:pk>/delete/", CVDeleteView.as_view(), name="cv_delete"),
     path("cv/<int:pk>/download/", CVListView.as_view(), name="cv_download"),
 ]
