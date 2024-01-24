012cae766c21f0cefdb8fa1a12ca59b4590a7c33
Johan Suarez <jhoanseb1992@gmail.com>
2024-01-24 22:14:31 +1100

    fixing detail and list template in cv app

--- cv/views.py
+++ cv/views.py
@@ -12,11 +12,31 @@ from cv.forms import CVForm, CVTemplateForm
 
 
 class CVListView(ListView):
-
     """CV List View"""
 
     template_name = "cv/list.html"
     model = CV
+    context_object_name = "cvs"
+
+    def get_queryset(self):
+        """Return the cvs for the current user."""
+        return CV.objects.filter(user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        """Add the user to the context."""
+        context = super().get_context_data(**kwargs)
+        instance = context["cvs"]
+        col_1 = []
+        col_2 = []
+        for i in range(len(instance)):
+            if i % 2 == 0:
+                col_1.append(instance[i])
+            else:
+                col_2.append(instance[i])
+
+        context["col_1"] = col_1
+        context["col_2"] = col_2
+        return context
 
 
 class CVDetailView(DetailView):
@@ -25,6 +45,13 @@ class CVDetailView(DetailView):
     template_name = "cv/detail.html"
     model = CV
     pk_url_kwarg = "pk"
+    context_object_name = "cv"
+
+    def get_context_data(self, **kwargs):
+        """Add the user to the context."""
+        context = super().get_context_data(**kwargs)
+
+        return context
 
 
 class CVCreateView(CreateView):
@@ -52,11 +79,30 @@ class CVCreateView(CreateView):
 class CVUpdateView(UpdateView):
     """CV Update View"""
 
-    template_name = "cv/update.html"
-    model = CV
-    fields = "__all__"
+    template_name = "cv/edit.html"
     success_url = reverse_lazy("cv:cv_list")
     pk_url_kwarg = "pk"
+    form_class = CVForm
+
+    def get_form(self, form_class=None):
+
+
+    def get_queryset(self):
+        """Return the cvs for the current user."""
+        return CV.objects.filter(user=self.request.user)
+
+    def form_valid(self, form):
+        """If the form is valid, save the associated model."""
+        form.instance.user = self.request.user
+        self.object = form.save()
+
+        return super().form_valid(form)
+
+    def get_form_kwargs(self):
+        """Return the keyword arguments for instantiating the form."""
+        kwargs = super().get_form_kwargs()
+        kwargs["user"] = self.request.user
+        return kwargs
 
 
 class CVDeleteView(DeleteView):
